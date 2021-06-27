7e5b895a446f8f96c53edb17a28f0ba6741dd4c5
hsgwKei <63716217+hsgwKei@users.noreply.github.com>
2021-06-27 17:09:51 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,58 @@
+const player = document.getElementById('video');
+const download = document.getElementById('download');
+const p_text = document.getElementById('text');
+var detections_json = "No Data";
+var save_arr = [];
+let labels = [
+  'UnixTime(ms)',
+  'face_score', 'face_boundingBox_x', 'face_boundingBox_y', 'face_boundingBox_width', 'face_boundingBox_height'
+]
+
+/**カメラを用いたビデオストリーミング**/
+function startVideo() {
+  var constraints = {
+    audio: true,
+    video: {
+      width: player.width,
+      height: player.height
+    }
+  };
+  navigator.mediaDevices.getUserMedia(constraints)
+  .then(function(stream) {
+    player.srcObject = stream;
+    player.onloadedmetadata = function(e) {
+      player.play();
+    };
+  })
+  .catch(function(err) {
+    console.log(err.name+": "+err.message);
+  });
+};
+
+window.onload = function () {
+  p_text.textContent = "before video on";
+  startVideo();
+}
+
+/**カメラオン時のイベント**/
+player.addEventListener('play', () => {
+  const model = facemesh.load();
+  setInterval(async () => {
+    const faces = model.estimateFaces(player);
+    p_text.textContent = JSON.stringify(faces);
+  }, 1000)
+  .catch((e) => {
+    console.log('setIntervalでエラー：'+e);
+  });
+})
+.catch((e) => {
+  console.log('player.addEventListenerでエラー：'+e);
+});
+
+/** jsonファイルのダウンロード **/
+function handleDownload() {
+  var blob = new Blob([ detections_json ], { "type" : "text/plain" });
+  var url = window.URL.createObjectURL(blob);
+  download.href = url;
+  window.navigator.msSaveBlob(blob, "test.json"); 
+}
